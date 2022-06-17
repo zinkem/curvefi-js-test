7e2b0bf42db727db67e5517dce4d663893288d2e
Matthew Zinke <zinkem@gmail.com>
2022-06-17 13:04:05 -0700

    add example code

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
 import curve from "@curvefi/api"
+import keys from "./config.json"
 
-console.log(curve);
+console.log(keys);
+
+const PRO_ID : String = keys.project_secret;
+
+(async () => {
+    // 1. Dev
+    // await curve.init('JsonRpc', {url: 'http://localhost:8545/', privateKey: ''}, { gasPrice: 0, maxFeePerGas: 0, maxPriorityFeePerGas: 0, chainId: 1 });
+    // OR
+    // await curve.init('JsonRpc', {}, {}); // In this case JsonRpc url, privateKey, fee data and chainId will be specified automatically
+
+    // 2. Infura
+    curve.init("Infura", { network: "homestead", apiKey: "PRO_ID" }, { chainId: 1 });
+    
+    // 3. Web3 provider
+    // curve.init('Web3', { externalProvider: <WEB3_PROVIDER> }, { chainId: 1 });
+    
+    // Fetch factory pools
+    await curve.fetchFactoryPools();
+    await curve.getCryptoFactoryPoolList();
+})()
